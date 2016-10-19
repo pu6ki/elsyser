3aaca166cfdfc0133e298f0aa5056ab79dce429a
wencakisa <wencakisa@gmail.com>
2016-10-19 21:13:44 +0300

    Every URL now has its own view

--- exams/views.py
+++ exams/views.py
@@ -1,12 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from django.views.generic import DetailView
 
-from .models import Exam
+from .models import Class, Subject, Exam
 
 
-def exams_list(request):
-    exams = Exam.objects.all().order_by('-date')
+def _exams_view(request, exams):
     paginator = Paginator(exams, 10)
 
     page = request.GET.get('page')
@@ -21,6 +19,20 @@ def exams_list(request):
     return render(request, 'exams/list.html', {'exams': exams})
 
 
-class ExamDetailView(DetailView):
-    model = Exam
-    template_name = 'exams/detail.html'
+def all_exams_list(request):
+    exams = Exam.objects.all().order_by('-date')
+
+    return _exams_view(request, exams)
+
+
+def all_class_exams_list(request, class_number):
+    exams = Exam.objects.filter(clazz__number=class_number)
+
+    return _exams_view(request, exams)
+
+
+def class_exam_list(request, class_number, class_title):
+    clazz = Class.objects.filter(number=class_number, title=class_title)
+    exams = Exam.objects.filter(clazz=clazz).order_by('-date')
+
+    return _exams_view(request, exams)
