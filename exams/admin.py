c4f54e84bbcd2b81ceff839caeb93e52b0c02509
wencakisa <wencakisa@gmail.com>
2016-10-21 19:50:36 +0300

    Registered new models in admin

--- exams/admin.py
+++ exams/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Class, Subject, Exam
+from .models import Teacher, Subject, Class, Exam
 
-admin.site.register(Class)
+admin.site.register(Teacher)
 admin.site.register(Subject)
+admin.site.register(Class)
 admin.site.register(Exam)
