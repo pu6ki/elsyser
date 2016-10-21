772a38c98ba4200776b758d7d8a08ae1c8d9ef07
wencakisa <wencakisa@gmail.com>
2016-10-21 19:50:22 +0300

    Added new models

--- exams/models.py
+++ exams/models.py
@@ -1,6 +1,24 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 
+# TODO: Update database to have valid teacher for each Subject
+
+class Teacher(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return '{} {}'.format(self.first_name, self.last_name)
+
+
+class Subject(models.Model):
+    title = models.CharField(max_length=50)
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE, default=1)
+
+    def __str__(self):
+        return self.title
+
+
 class Class(models.Model):
     number = models.PositiveSmallIntegerField(
         default=8,
@@ -13,23 +31,24 @@ class Class(models.Model):
         )
     )
 
+    class Meta:
+        ordering = ['number', 'title']
+
     def __str__(self):
         return '{}{}'.format(self.number, self.title)
 
-class Subject(models.Model):
-    title = models.CharField(max_length=30)
-
-    def __str__(self):
-        return self.title
 
 class Exam(models.Model):
     subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
     date = models.DateField(auto_now=False)
     clazz = models.ForeignKey(Class, on_delete=models.CASCADE)
 
+    class Meta:
+        ordering = ['-date', 'subject', 'clazz']
+
     def __str__(self):
         return '{} - {} ({})'.format(
-            self.subject.title,
+            self.subject,
             self.clazz,
             self.date.strftime('%d-%m-%Y')
         )
