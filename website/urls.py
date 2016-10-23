7516b6141569b0cf10fce3e11a357b91f6633aa6
wencakisa <wencakisa@gmail.com>
2016-10-23 17:39:03 +0300

    Added django authentication framework urls

--- website/urls.py
+++ website/urls.py
@@ -1,7 +1,12 @@
 from django.conf.urls import url, include
 
+from .views import home, register, profile
+
 
 app_name = 'website'
 urlpatterns = [
-    url(r'^exams/', include('exams.urls')),
+    url(r'^$', home, name='home'),
+    url(r'^register/$', register, name='register'),
+    url(r'^accounts/profile/$', profile, name='profile'),
+
 ]
