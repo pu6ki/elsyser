c06d39b3b72a15c7a3c967867245d61bcce686d6
wencakisa <wencakisa@gmail.com>
2016-10-23 17:40:03 +0300

    Generic views

--- exams/views.py
+++ exams/views.py
@@ -1,29 +1,34 @@
 from datetime import datetime, timedelta
 
-from rest_framework import generics
+from django.views.generic import ListView
 
 from .models import Exam, Class
 from .serializers import ExamsSerializer
 
 
-class ExamsList(generics.ListCreateAPIView):
+class ExamsList(ListView):
 
-    queryset = Exam.objects.filter(date__lte=datetime.now()+timedelta(days=5))
-    serializer_class = ExamsSerializer
-
-
-class AllClassExamsList(generics.ListAPIView):
-
-    serializer_class = ExamsSerializer
+    model = Exam
+    template_name = 'exams/list.html'
+    context_object_name = 'exams'
+    paginate_by = 5
 
     def get_queryset(self):
-        return Exam.objects.filter(
+        return self.model.objects.filter(
+            date__lte=datetime.now()+timedelta(days=5)
+        )
+
+
+class AllClassExamsList(ExamsList):
+
+    def get_queryset(self):
+        return self.model.objects.filter(
             date__lte=datetime.now()+timedelta(days=5),
             clazz__number=self.kwargs['class_number'],
         )
 
 
-class CertainClassExamsList(AllClassExamsList):
+class CertainClassExamsList(ExamsList):
 
     def get_queryset(self):
         clazz = Class.objects.get(
@@ -31,7 +36,7 @@ class CertainClassExamsList(AllClassExamsList):
             title=self.kwargs['class_title'],
         )
 
-        return Exam.objects.filter(
+        return self.model.objects.filter(
             date__lte=datetime.now()+timedelta(days=5),
             clazz=clazz,
         )
