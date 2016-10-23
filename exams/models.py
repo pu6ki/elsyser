15e6ac71a926e50472719c7f5e62367f028e5226
wencakisa <wencakisa@gmail.com>
2016-10-23 17:38:36 +0300

    Fixes

--- exams/models.py
+++ exams/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.core.exceptions import ValidationError
+from django.contrib.auth.models import User
 
 from datetime import datetime
 
