b3fcd121c51ee88cc3830a1a8e233dbbbcfafa83
wencakisa <wencakisa@gmail.com>
2016-10-24 22:10:49 +0300

    After register -> login

--- students/serializers.py
+++ students/serializers.py
@@ -1,5 +1,9 @@
 from django.contrib.auth.models import User, Group
+from django.contrib.auth import login
+
 from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
+from rest_framework.authtoken.models import Token
 
 
 class StudentSerializer(serializers.ModelSerializer):
@@ -9,18 +13,28 @@ class StudentSerializer(serializers.ModelSerializer):
         required=True,
         min_length=6,
         style={
-            'input_type': 'password'
+            'input_type': 'password',
         },
         error_messages={
-            "blank": "Password cannot be empty.",
-            "min_length": "Password too short.",
+            'blank': 'Password cannot be empty.',
+            'min_length': 'Password too short.',
         },
+    )
+
+    email = serializers.EmailField(
+        max_length=100,
+        validators=[
+            UniqueValidator(
+                queryset=User.objects.all(),
+                message='Student with this email already exists.'
+            )
+        ],
     )
 
     class Meta:
         model = User
         fields = (
-            'id', 'username', 'password', 'first_name', 'last_name', 'email',
+            'id', 'username', 'first_name', 'last_name', 'email', 'password',
         )
         extra_kwargs = {
             'id': {'read_only': True},
@@ -28,21 +42,27 @@ class StudentSerializer(serializers.ModelSerializer):
         }
 
     def create(self, validated_data):
-        first_name = validated_data['first_name']
-        last_name = validated_data['last_name']
+        first_name = validated_data.pop('first_name')
+        last_name = validated_data.pop('last_name')
 
         username = first_name + '_' + last_name
+
+        email = validated_data.pop('email')
 
         user = User(
             username=username,
             first_name=first_name,
             last_name=last_name,
-            email=validated_data['email'],
+            email=email,
         )
 
-        user.set_password(validated_data['password'])
+        user.set_password(validated_data.pop('password'))
 
         user.save()
+
         Group.objects.get(name='Students').user_set.add(user)
+        Token.objects.create(user=user)
+
+        login(self.context['request'], user)
 
         return user
