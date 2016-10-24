b2001dd91896c7bc9fff43b9f9d259792cfbbc76
wencakisa <wencakisa@gmail.com>
2016-10-24 22:09:57 +0300

    Added register and profile urls

--- students/urls.py
+++ students/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url, include
 
-from .views import StudentRegistration
+from .views import StudentRegistration, StudentLogin, StudentProfile
 
 app_name = 'students'
 urlpatterns = [
     url(r'^register/', StudentRegistration.as_view(), name='register'),
+    url(r'^login/', StudentLogin.as_view(), name='login'), # Under construction!!!
+    url(r'^profile/', StudentProfile.as_view(), name='profile'),
 ]
