533aefe50a916d0d44d9dae4088e57c4814bb82d
wencakisa <wencakisa@gmail.com>
2016-10-24 22:10:35 +0300

    Register and profile views

--- students/views.py
+++ students/views.py
@@ -1,4 +1,7 @@
+from django.contrib.auth.models import User
+
 from rest_framework import generics
+from rest_framework.response import Response
 
 from .serializers import StudentSerializer
 
@@ -6,3 +9,19 @@ from .serializers import StudentSerializer
 class StudentRegistration(generics.CreateAPIView):
 
     serializer_class = StudentSerializer
+
+
+class StudentLogin(generics.CreateAPIView):
+
+    serializer_class = StudentSerializer
+
+
+class StudentProfile(generics.RetrieveAPIView):
+
+    serializer_class = StudentSerializer
+
+    def get(self, request, format=None):
+        student = User.objects.get(username=request.user)
+        serializer = StudentSerializer(student)
+
+        return Response(serializer.data)
