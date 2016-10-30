81d1d84ed9960b6f54bda455b721a0971fc089d3
wencakisa <wencakisa@gmail.com>
2016-10-30 10:35:45 +0200

    Registered News model in Admin

--- students/admin.py
+++ students/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from .models import Class, Student, Subject, Exam
+from .models import Class, Student, Subject, Exam, News
 
 admin.site.register(Class)
 admin.site.register(Student)
 admin.site.register(Subject)
 admin.site.register(Exam)
+admin.site.register(News)
