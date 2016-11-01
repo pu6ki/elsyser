7b5976cc8151284e7184259cd9b7f36fb1e63c98
wencakisa <wencakisa@gmail.com>
2016-11-01 11:34:18 +0200

    Fixed urls routes

--- students/urls.py
+++ students/urls.py
@@ -5,8 +5,8 @@ from .views import StudentRegistration, StudentProfile, ExamsList, NewsList
 
 app_name = 'students'
 urlpatterns = [
-    url(r'^register/', StudentRegistration.as_view(), name='register'),
-    url(r'^profile/', StudentProfile.as_view(), name='profile'),
-    url(r'^exams/', ExamsList.as_view(), name='exams'),
-    url(r'^news/', NewsList.as_view(), name='news'),
+    url(r'^register/$', StudentRegistration.as_view(), name='register'),
+    url(r'^profile/$', StudentProfile.as_view(), name='profile'),
+    url(r'^exams/$', ExamsList.as_view(), name='exams'),
+    url(r'^news/$', NewsList.as_view(), name='news'),
 ]
