8e5ba7b761258a7b5fbee5d14d44ed81492f5044
matir8 <martin.markov28@gmail.com>
2016-11-02 19:04:05 +0200

    Shit happens

--- elsyser/settings.py
+++ elsyser/settings.py
@@ -108,14 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 #Necessary in order to return auth token, but it breaks the register
-REST_FRAMEWORK = {
-   'DEFAULT_AUTHENTICATION_CLASSES': (
-       'rest_framework.authentication.TokenAuthentication',
-   ),
-   'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAdminUser'
-   ),
-}
+
 
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_METHODS = (
