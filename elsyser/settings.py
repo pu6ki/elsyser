f53ab0d433dd5fdf699f01d4b578b0ef0fbb6062
wencakisa <wencakisa@gmail.com>
2016-11-06 12:08:39 +0200

    Added MEDIA_ROOT

--- elsyser/settings.py
+++ elsyser/settings.py
@@ -137,4 +137,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 STATIC_URL = '/static/'
