3e7b15138e6b7014e9b594d6284e71e41d7ddf75
wencakisa <wencakisa@gmail.com>
2016-11-07 19:43:14 +0200

    Added homework material file format validator

--- students/validators.py
+++ students/validators.py
@@ -1,5 +1,7 @@
 from django.core.exceptions import ValidationError
+
 from datetime import datetime
+import os
 
 
 def validate_date(date):
@@ -7,3 +9,12 @@ def validate_date(date):
         raise ValidationError('Only future dates allowed.')
     if date.weekday() not in range(0, 5):
         raise ValidationError('Exam can be done only from Monday to Friday.')
+
+def validate_file_extension(value):
+    ext = os.path.splitext(value.name)[1]
+    valid_extensions = ['.zip', '.rar']
+
+    if not ext.lower() in valid_extensions:
+        raise ValidationError(
+            'Allowed file extensions are: {}'.format(','.join(valid_extensions))
+        )
