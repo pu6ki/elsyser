850b2d9ef169b2a3fdd94d33fde0de936619a8a0
matir8 <martin.markov28@gmail.com>
2016-11-12 14:53:27 +0200

    Refreshing comments now works

--- web-client/controllers/NewsControllers/DetailedNewsController.js
+++ web-client/controllers/NewsControllers/DetailedNewsController.js
@@ -1,12 +1,12 @@
 import { requester } from '../../utils/requster.js';
 import { templates } from '../../utils/templates.js';
 
-export function DetailedNewsController(id) {
-    let dataFromAPI;
-    return new Promise((resolve, reject) => {
-        let newsUrl = `http://127.0.0.1:8000/api/news/${id}/`;
+let dataFromAPI;
+let newsUrl = "http://127.0.0.1:8000/api/news/";
 
-        resolve(requester.getJSON(newsUrl));
+export function DetailedNewsController(id) {
+    return new Promise((resolve, reject) => {
+        resolve(requester.getJSON(newsUrl + id + '/'));
     }).then((data) => {
         dataFromAPI = data;
         return new Promise((resolve, reject) => {
@@ -20,5 +20,21 @@ export function DetailedNewsController(id) {
         console.log(err);
     });
 
-    //TODO: Add comment field
+
+}
+
+export function loadNews(id) {
+    console.log('req');
+    requester.getJSON(newsUrl + id + '/')
+        .then((data) => {
+            if (data.comment_set.length > dataFromAPI.comment_set.length) {
+                templates.get('partials/comment')
+                    .then((res) => {
+                        let hbTemplate = Handlebars.compile(res),
+                            template = hbTemplate(data.comment_set[dataFromAPI.comment_set.length]);
+                        $('#comments').append(template);
+                        dataFromAPI = data;
+                    });
+            }
+        });
 }
--- web-client/controllers/NewsControllers/NewsController.js
+++ web-client/controllers/NewsControllers/NewsController.js
@@ -1,11 +1,11 @@
 import { requester } from '../../utils/requster.js';
 import { templates } from '../../utils/templates.js';
 
+const newsUrl = 'http://127.0.0.1:8000/api/news/';
+
 export function NewsController() {
     let dataFromAPI;
     return new Promise((resolve, reject) => {
-        let newsUrl = 'http://127.0.0.1:8000/api/news/';
-
         resolve(requester.getJSON(newsUrl));
     }).then((data) => {
         dataFromAPI = data;
