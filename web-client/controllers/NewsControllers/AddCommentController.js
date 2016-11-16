3ff1590836ca5e55a7a30c50810ef5f3effc8347
matir8 <martin.markov28@gmail.com>
2016-11-16 20:10:52 +0200

    Added separated controller about adding comments

---
+++ web-client/controllers/NewsControllers/AddCommentController.js
@@ -0,0 +1,24 @@
+import { requester } from '../../utils/requster.js';
+import { templates } from '../../utils/templates.js';
+import { validator } from '../../utils/validator.js';
+
+export function AddCommentController(id) {
+    let body = {
+        content: ''
+    },
+        addCommentUrl = `http://127.0.0.1:8000/api/news/${id}/comments/`;
+
+    if (validator.comment($('#comment-content').val())) {
+        body.content = $('#comment-content').val();
+        requester.postJSON(addCommentUrl, body)
+            .then(() => {
+                toastr.success('Comment added!');
+                $('#comment-content').val('');
+            }).catch((err) => {
+                toastr.error('Comments can\' be empty!')
+            })
+    }
+    else {
+        toastr.error('Comments shold be max 2048 characters long!');
+    }
+}
