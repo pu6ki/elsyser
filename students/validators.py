f0db20802f8de19caa2bb00a209e278fb4ed2778
wencakisa <wencakisa@gmail.com>
2016-11-20 10:32:37 +0200

    Added materials to homeworks client view

--- students/validators.py
+++ students/validators.py
@@ -1,7 +1,6 @@
 from django.core.exceptions import ValidationError
 
 from datetime import datetime
-import os
 
 
 def validate_date(date):
@@ -9,13 +8,3 @@ def validate_date(date):
         raise ValidationError('Only future dates allowed.')
     if date.weekday() not in range(0, 5):
         raise ValidationError('Weekend days are not valid.')
-
-
-def validate_file_extension(value):
-    ext = os.path.splitext(value.name)[1]
-    valid_extensions = ['.zip', '.rar']
-
-    if not ext.lower() in valid_extensions:
-        raise ValidationError(
-            'Allowed file extensions are: {}'.format(','.join(valid_extensions))
-        )
