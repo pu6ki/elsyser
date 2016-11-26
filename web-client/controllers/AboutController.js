5f3b0d5e7c1282a113f7e8c83f5255a6fcc42122
matir8 <martin.markov28@gmail.com>
2016-11-26 10:31:15 +0200

    Moved the client to another repo

--- web-client/controllers/AboutController.js
+++
@@ -1,9 +0,0 @@
-import { requester } from '../utils/requster.js';
-import { templates } from '../utils/templates.js';
-
-export function AboutController() {
-    templates.get('about')
-        .then((template) => {
-            $('#content').html(template);
-        });
-}
--- web-client/controllers/HomeController.js
+++
@@ -1,9 +0,0 @@
-import { requester } from '../utils/requster.js';
-import { templates } from '../utils/templates.js';
-
-export function HomeController() {
-    templates.get('home')
-        .then((template) => {
-            $('#content').html(template);
-        });
-}
