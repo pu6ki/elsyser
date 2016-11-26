620a36c2601dc47f75e3603e34b34788f7aa7313
wencakisa <wencakisa@gmail.com>
2016-11-26 10:48:15 +0200

    edited db options

--- elsyser/settings.py
+++ elsyser/settings.py
@@ -141,9 +141,9 @@ USE_TZ = True
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+# import dj_database_url
+# db_from_env = dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(db_from_env)
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
