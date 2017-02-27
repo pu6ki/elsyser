48b7367df97ff3fd00fe26f4afaceed64570350d
wencakisa <wencakisa@gmail.com>
2017-02-27 15:55:32 +0200

    Implemented tests for grading

--- students/tests.py
+++ students/tests.py
@@ -5,7 +5,7 @@ from rest_framework.reverse import reverse
 from rest_framework import status
 from rest_framework.authtoken.models import Token
 
-from students.models import Class, Subject, Student, Teacher
+from students.models import Class, Subject, Student, Teacher, Grade
 from students.serializers import StudentProfileSerializer
 
 
@@ -373,4 +373,435 @@ class ProfileViewSetTestCase(APITestCase):
             format='json'
         )
 
+        self.assertEqual(request.status_code, status.HTTP_200_OK)
+
+class GradesListViewTestCase(APITestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.view_name = 'students:grades-list'
+
+        self.clazz = Class.objects.create(number=10, letter='A')
+        self.subject = Subject.objects.create(title='Maths')
+
+        self.user = User.objects.create(
+            username='tester',
+            first_name='test',
+            last_name='user',
+            email='tester@gmail.com',
+            password='pass'
+        )
+
+        self.student = Student.objects.create(
+            user=self.user,
+            clazz=self.clazz,
+            info='I am the lord of the rings.',
+            profile_image_url='http://www.shockmansion.com/wp-content/myimages/2016/03/rr231.jpg'
+        )
+
+        self.grade1 = Grade.objects.create(
+            value=5.67,
+            subject=self.subject,
+            student=self.student
+        )
+        self.grade2 = Grade.objects.create(
+            value=4.82,
+            subject=self.subject,
+            student=self.student
+        )
+
+    def test_list_with_anonymous_user(self):
+        request = self.client.get(
+            reverse(self.view_name, kwargs={'subject_pk': self.subject.id})
+        )
+
+        self.assertEqual(
+            request.data['detail'],
+            'Authentication credentials were not provided.'
+        )
+        self.assertEqual(request.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_list_with_authenticated_user(self):
+        self.client.force_authenticate(user=self.user)
+
+        request = self.client.get(
+            reverse(self.view_name, kwargs={'subject_pk': self.subject.id})
+        )
+
+        self.assertEqual(request.data[0]['value'], self.grade2.value)
+        self.assertEqual(request.data[1]['value'], self.grade1.value)
+        self.assertEqual(
+            request.data[0]['student']['user']['username'],
+            self.user.username
+        )
+        self.assertEqual(
+            request.data[1]['student']['user']['username'],
+            self.user.username
+        )
+        self.assertEqual(request.status_code, status.HTTP_200_OK)
+
+    def test_list_with_invalid_subject_id(self):
+        self.client.force_authenticate(user=self.user)
+        request = self.client.get(
+            reverse(self.view_name, kwargs={'subject_pk': self.subject.id - 1})
+        )
+
+        self.assertFalse(request.data)
+        self.assertEqual(request.status_code, status.HTTP_200_OK)
+
+class GradesDetailViewTestCase(APITestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.view_name = 'students:grades-detail'
+
+        self.clazz = Class.objects.create(number=10, letter='A')
+        self.subject1 = Subject.objects.create(title='Maths')
+        self.subject2 = Subject.objects.create(title='Literature')
+
+        self.user1 = User.objects.create(
+            username='tester',
+            first_name='test',
+            last_name='user',
+            email='tester@gmail.com',
+            password='pass'
+        )
+        self.user2 = User.objects.create(
+            username='tester1',
+            first_name='test1',
+            last_name='user1',
+            email='tester@abv.bg',
+            password='password123'
+        )
+        self.user3 = User.objects.create(
+            username='tester2',
+            first_name='test2',
+            last_name='user2',
+            email='tester@abv.com',
+            password='password123456'
+        )
+
+        self.student1 = Student.objects.create(
+            user=self.user1,
+            clazz=self.clazz,
+            info='I am the lord of the rings.',
+            profile_image_url='http://www.shockmansion.com/wp-content/myimages/2016/03/rr231.jpg'
+        )
+        self.student2 = Student.objects.create(
+            user=self.user2,
+            clazz=self.clazz,
+            info='information'
+        )
+
+        self.teacher = Teacher.objects.create(
+            user=self.user3,
+            subject=self.subject1
+        )
+
+        self.grade1 = Grade.objects.create(
+            value=5.67,
+            subject=self.subject1,
+            student=self.student1
+        )
+        self.grade2 = Grade.objects.create(
+            value=4.82,
+            subject=self.subject1,
+            student=self.student2
+        )
+
+    def test_grade_detail_with_anonymous_user(self):
+        request = self.client.get(
+            reverse(
+                self.view_name,
+                kwargs={
+                    'subject_pk': self.subject1.id,
+                    'user_pk': self.user1.id
+                }
+            )
+        )
+
+        self.assertEqual(
+            request.data['detail'],
+            'Authentication credentials were not provided.'
+        )
+        self.assertEqual(request.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_grade_detail_with_authenticated_user(self):
+        self.client.force_authenticate(user=self.user1)
+
+        request = self.client.get(
+            reverse(
+                self.view_name,
+                kwargs={
+                    'subject_pk': self.subject1.id,
+                    'user_pk': self.user1.id
+                }
+            )
+        )
+
+        self.assertEqual(request.data[0]['value'], self.grade1.value)
+        self.assertEqual(
+            request.data[0]['student']['user']['username'],
+            self.user1.username
+        )
+        self.assertEqual(request.status_code, status.HTTP_200_OK)
+
+    def test_grade_detail_of_another_user(self):
+        self.client.force_authenticate(user=self.user2)
+
+        request = self.client.get(
+            reverse(
+                self.view_name,
+                kwargs={
+                    'subject_pk': self.subject1.id,
+                    'user_pk': self.user1.id
+                }
+            )
+        )
+
+        self.assertEqual(
+            request.data['message'],
+            'You can view only your own grades.'
+        )
+        self.assertEqual(request.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_grade_detail_with_non_graded_subject(self):
+        self.client.force_authenticate(user=self.user1)
+
+        request = self.client.get(
+            reverse(
+                self.view_name,
+                kwargs={
+                    'subject_pk': self.subject1.id - 1,
+                    'user_pk': self.user1.id
+                }
+            )
+        )
+
+        self.assertFalse(request.data)
+        self.assertEqual(request.status_code, status.HTTP_200_OK)
+
+    def test_grade_detail_with_invalid_user(self):
+        self.client.force_authenticate(user=self.user1)
+
+        request = self.client.get(
+            reverse(
+                self.view_name,
+                kwargs={
+                    'subject_pk': self.subject1.id,
+                    'user_pk': self.user1.id - 1
+                }
+            )
+        )
+
+        self.assertEqual(request.data['detail'], 'Not found.')
+        self.assertEqual(request.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_grade_posting_with_anonymous_user(self):
+        request = self.client.post(
+            reverse(
+                self.view_name,
+                kwargs={
+                    'subject_pk': self.subject1.id,
+                    'user_pk': self.user1.id
+                }
+            ),
+            data={'value': 3.12},
+            format='json'
+        )
+
+        self.assertEqual(
+            request.data['detail'],
+            'Authentication credentials were not provided.'
+        )
+        self.assertEqual(request.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_grade_posting_with_student(self):
+        self.client.force_authenticate(user=self.user1)
+
+        request = self.client.post(
+            reverse(
+                self.view_name,
+                kwargs={
+                    'subject_pk': self.subject1.id,
+                    'user_pk': self.user1.id
+                }
+            ),
+            data={'value': 3.12},
+            format='json'
+        )
+
+        self.assertEqual(
+            request.data['detail'],
+            'You do not have permission to perform this action.'
+        )
+        self.assertEqual(request.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_grade_posting_with_teacher(self):
+        self.client.force_authenticate(user=self.user3)
+
+        request = self.client.post(
+            reverse(
+                self.view_name,
+                kwargs={
+                    'subject_pk': self.subject1.id,
+                    'user_pk': self.user1.id
+                }
+            ),
+            data={'value': 3.12},
+            format='json'
+        )
+
+        self.assertEqual(request.status_code, status.HTTP_201_CREATED)
+
+    def test_grade_posting_with_invalid_subject_id(self):
+        self.client.force_authenticate(user=self.user3)
+
+        request = self.client.post(
+            reverse(
+                self.view_name,
+                kwargs={
+                    'subject_pk': self.subject1.id - 1,
+                    'user_pk': self.user1.id
+                }
+            ),
+            data={'value': 3.12},
+            format='json'
+        )
+
+        self.assertEqual(request.data['detail'], 'Not found.')
+        self.assertEqual(request.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_grade_posting_with_different_subject(self):
+        self.client.force_authenticate(user=self.user3)
+
+        request = self.client.post(
+            reverse(
+                self.view_name,
+                kwargs={
+                    'subject_pk': self.subject2.id,
+                    'user_pk': self.user1.id
+                }
+            ),
+            data={'value': 3.12},
+            format='json'
+        )
+
+        self.assertEqual(
+            request.data['message'],
+            'You can only post grades for your subject.'
+        )
+        self.assertEqual(request.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_grade_posting_with_invalid_user_id(self):
+        self.client.force_authenticate(user=self.user3)
+
+        request = self.client.post(
+            reverse(
+                self.view_name,
+                kwargs={
+                    'subject_pk': self.subject1.id,
+                    'user_pk': self.user1.id - 1
+                }
+            ),
+            data={'value': 3.12},
+            format='json'
+        )
+
+        self.assertEqual(request.data['detail'], 'Not found.')
+        self.assertEqual(request.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_grade_posting_with_too_low_grade(self):
+        self.client.force_authenticate(user=self.user3)
+
+        request = self.client.post(
+            reverse(
+                self.view_name,
+                kwargs={
+                    'subject_pk': self.subject1.id,
+                    'user_pk': self.user1.id
+                }
+            ),
+            data={'value': 1.23},
+            format='json'
+        )
+
+        self.assertEqual(
+            request.data['value'],
+            ['Ensure this value is greater than or equal to 2.']
+        )
+        self.assertEqual(request.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_grade_posting_with_too_high_grade(self):
+        self.client.force_authenticate(user=self.user3)
+
+        request = self.client.post(
+            reverse(
+                self.view_name,
+                kwargs={
+                    'subject_pk': self.subject1.id,
+                    'user_pk': self.user1.id
+                }
+            ),
+            data={'value': 6.81},
+            format='json'
+        )
+
+        self.assertEqual(
+            request.data['value'],
+            ['Ensure this value is less than or equal to 6.']
+        )
+        self.assertEqual(request.status_code, status.HTTP_400_BAD_REQUEST)
+
+class StudentsListViewTestCase(APITestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.view_name = 'students:students-list'
+
+        self.clazz = Class.objects.create(number=10, letter='A')
+        self.subject = Subject.objects.create(title='Maths')
+
+        self.user = User.objects.create(
+            username='tester',
+            first_name='test',
+            last_name='user',
+            email='tester@gmail.com',
+            password='pass'
+        )
+
+        self.student = Student.objects.create(
+            user=self.user,
+            clazz=self.clazz,
+            info='I am the lord of the rings.'
+        )
+
+    def test_students_list_with_anonymous_user(self):
+        request = self.client.get(
+            reverse(
+                self.view_name,
+                kwargs={
+                    'class_number': self.clazz.number,
+                    'class_letter': self.clazz.letter
+                }
+            )
+        )
+
+        self.assertEqual(
+            request.data['detail'],
+            'Authentication credentials were not provided.'
+        )
+        self.assertEqual(request.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_students_list_with_authenticated_user(self):
+        self.client.force_authenticate(user=self.user)
+
+        request = self.client.get(
+            reverse(
+                self.view_name,
+                kwargs={
+                    'class_number': self.clazz.number,
+                    'class_letter': self.clazz.letter
+                }
+            )
+        )
+
+        self.assertTrue(request.data)
         self.assertEqual(request.status_code, status.HTTP_200_OK)
