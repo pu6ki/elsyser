c01323ea07e2551651a5622d642c4c4a4caf1625
wencakisa <wencakisa@gmail.com>
2017-06-03 12:33:32 +0300

    Little refactoring on news urls

--- news/urls.py
+++ news/urls.py
@@ -10,9 +10,7 @@ from .views import (
 
 app_name = 'news'
 
-students_router = routers.SimpleRouter()
-students_router.register(r'news/students', NewsStudentsViewSet, base_name='studentsNews')
-
+students_url_pattern = r'news/students'
 teachers_default_url_pattern = r'news/teachers'
 teachers_detail_url_pattern = r'(?P<class_number>[8]|[9]|1[0-2])/(?P<class_letter>[A-Z])'
 teachers_url_pattern = r'{default_pattern}/{detail_pattern}'.format(
@@ -20,20 +18,21 @@ teachers_url_pattern = r'{default_pattern}/{detail_pattern}'.format(
     detail_pattern=teachers_detail_url_pattern
 )
 
+students_router = routers.SimpleRouter()
+students_router.register(students_url_pattern, NewsStudentsViewSet, base_name='studentsNews')
+
 teachers_router = routers.SimpleRouter()
 teachers_router.register(teachers_url_pattern, NewsTeachersViewSet, base_name='teachersNews')
 
 students_comments_router = routers.NestedSimpleRouter(
-    students_router, r'news/students', lookup='studentsNews'
+    students_router, students_url_pattern, lookup='studentsNews'
 )
 students_comments_router.register(
     r'comments', CommentsViewSet, base_name='studentsNews-comments'
 )
 
 teachers_comments_router = routers.NestedSimpleRouter(
-    teachers_router,
-    teachers_url_pattern,
-    lookup='teachersNews'
+    teachers_router, teachers_url_pattern, lookup='teachersNews'
 )
 teachers_comments_router.register(r'comments', CommentsViewSet, base_name='teachersNews-comments')
 
