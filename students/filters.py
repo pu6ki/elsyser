97fa556ab50972d7b50f08687942160f6a65f0ef
wencakisa <wencakisa@gmail.com>
2017-08-11 15:15:49 +0300

    Changed grades filter

--- students/filters.py
+++ students/filters.py
@@ -3,4 +3,14 @@ from rest_framework import filters
 
 class GradeFilterBackend(filters.BaseFilterBackend):
     def filter_queryset(self, request, queryset, view):
-        return queryset.filter(subject__id=view.kwargs['subject_pk']).order_by('-pk')
+        class_letter = request.query_params.get('class_letter', '')
+        class_number = request.query_params.get('class_number')
+
+        queryset = queryset.filter(subject__id=view.kwargs['subject_pk']).order_by('-pk')
+
+        if class_letter:
+            queryset = queryset.filter(student__clazz__letter=class_letter)
+        if class_number:
+            queryset = queryset.filter(student__clazz__number=class_number)
+
+        return queryset
