b3490e633386523b2f93cfeac5d3aa808439160c
wencakisa <wencakisa@gmail.com>
2017-08-23 11:38:08 +0300

    Added DEFAULT_AUTHENTICATION_CLASSES in DRF settings

--- elsyser/settings.py
+++ elsyser/settings.py
@@ -180,5 +180,9 @@ EMAIL_USE_TLS = True
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 5
+    'PAGE_SIZE': 5,
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    )
 }
--- elsyser/urls.py
+++ elsyser/urls.py
@@ -1,6 +1,6 @@
+from django.conf import settings
 from django.conf.urls import url, include
 from django.conf.urls.static import static
-from django.conf import settings
 from django.contrib import admin
 
 urlpatterns = [
@@ -8,4 +8,4 @@ urlpatterns = [
     url(r'^api-auth/', include('rest_framework.urls')),
     url(r'^docs/', include('rest_framework_docs.urls')),
     url(r'^api/', include('elsyser.api')),
-]+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
