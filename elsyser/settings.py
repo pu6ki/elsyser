1af09e5c584fbd52f07aa618091d8932b36fdca9
wencakisa <wencakisa@gmail.com>
2017-10-02 18:42:09 +0300

    Removed password retype on change

--- elsyser/settings.py
+++ elsyser/settings.py
@@ -196,7 +196,6 @@ REST_FRAMEWORK = {
 DJOSER = {
     'DOMAIN': 'elsyser.netlify.com',
     'SITE_NAME': 'ELSYSER',
-    'SET_PASSWORD_RETYPE': True,
     'PASSWORD_RESET_CONFIRM_URL': '#/auth/password/reset/confirm/{uid}/{token}',
     'PASSWORD_RESET_SHOW_EMAIL_NOT_FOUND': True
 }
