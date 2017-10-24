34bcf9be1fe90e05c8e1a865ded463ad86430be9
wencakisa <wencakisa@gmail.com>
2017-10-24 19:04:58 +0300

    Fixed older unit tests

--- homeworks/tests.py
+++ homeworks/tests.py
@@ -321,7 +321,7 @@ class SubmissionsViewSetTestCase(APITestCase):
         )
 
         self.assertNotEqual(
-            response.data['results'][0],
+            response.data,
             SubmissionSerializer(self.student2_submission).data
         )
         self.assertEqual(response.status_code, status.HTTP_200_OK)
@@ -371,10 +371,8 @@ class SubmissionsViewSetTestCase(APITestCase):
             )
         )
 
-        results = response.data['results']
-
-        self.assertEqual(results[1]['id'], self.student1_submission.id)
-        self.assertEqual(results[0]['id'], self.student2_submission.id)
+        self.assertEqual(response.data[1]['id'], self.student1_submission.id)
+        self.assertEqual(response.data[0]['id'], self.student2_submission.id)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_submissions_detail_with_teacher_user(self):
@@ -427,7 +425,7 @@ class SubmissionsViewSetTestCase(APITestCase):
             )
         )
 
-        self.assertEqual(response.data['results'], [])
+        self.assertEqual(response.data, [])
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_submission_creation_with_teacher_user(self):
